b4ead2e100b22d74941df940413bffc167da52d7
obisian RK <obisianr@gmail.com>
2024-04-13 19:59:48 +0530

    assets file added

---
+++ assets/js/typewriter.js
@@ -0,0 +1,11 @@
+/* Typewriter */
+const instance = new Typewriter('#typewriter', {
+    strings: ['The developer', 'The  designer'],
+  	autoStart: true,
+  	loop: true,
+});
+const about = new Typewriter('#about', {
+    strings: ['MANOJ', 'Mr. MAD'],
+  	autoStart: true,
+  	loop: true,
+});
